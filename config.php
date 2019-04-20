261f8e377c6d76d2590b64745c6ff90dbeb1b08b
Anthony <adshen@gmail.com>
2019-04-20 22:27:03 +0800

    fix a bug of the permission

--- config.php
+++ config.php
@@ -9,38 +9,41 @@
 
 return [
 
+    //must use absolute path
+    'logs_dir'   => '/path/of/the/logs',
+
+
     //php user name and group
     'server_user_name' => [
         'user'       => 'www',
         'user_group' => 'www',
     ],
 
+    'record_request_data'   => false,
+
+    'only_pull_master'  => true,
+
+    'timezone'  => 'PRC',
+
     //set default_project
-    'default_project' => false,
+    'default_project'       => true,
     'default_project_name'  => 'default',
 
     //projects name and secret which set in github
     'project' => [
-        'default' => [
+        'default'   => [
             'dir'       => '/home/www/default',
-            'secret'    => '123456',
+            'secret'    => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
         ],
-        'project1' => [
-            'dir'       => '/home/www/api',
-            'secret'    => '123456',
-        ],
-        'project2' => [
-            'dir'       => '/home/www/web',
-            'secret'    => '123456',
-        ],
+//        'project1' => [
+//            'dir'       => '/home/www/project1',
+//            'secret'    => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+//        ],
+//        'project2' => [
+//            'dir'       => '/home/www/project2',
+//            'secret'    => 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+//        ],
     ],
 
-    'record_request_data' => true,
-
-    'only_pull_master' => true,
-
-    'logs_dir'   => '../logs',
-
-    'timezone'   => 'PRC',
 
 ];
